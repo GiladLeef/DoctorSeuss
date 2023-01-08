1a71a73048b1f5d8f3de00c6e390d755a5b56234
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2023-01-08 15:46:16 +0200

    Added graphical web interface

---
+++ web-interface/src/components/Credentials.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="card new-card"
+    style="width: 500px">
+    <div class="card-content">
+      <p class="title">{{ $t('dashboard.intents.credentials.title') }}</p>
+
+      <b-field :label="$t('dashboard.intents.credentials.token.title')">
+        <b-input
+          v-model="token"
+          type="text"
+          rounded
+          :placeholder="$t('dashboard.intents.credentials.token.text')"
+          required></b-input>
+      </b-field>
+
+      <button
+        class="button is-primary is-rounded"
+        @click="saveToken()">
+        <strong>
+          {{ $t('generic.save') }}
+        </strong>
+      </button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        token:  localStorage.getItem('Doctor Seuss-Token')
+      }
+    },
+    methods: {
+      saveToken() {
+        localStorage.setItem('Doctor Seuss-Token', this.token)
+        this.$parent.close()
+      }
+    }
+  }
+</script>
---
+++ web-interface/src/components/LocaleSwitch.vue
@@ -0,0 +1,58 @@
+<template>
+  <b-dropdown aria-role="list">
+    <a
+      slot="trigger"
+      :class="{ 'navbar-item': navbar }">
+      <b-icon
+        class="media-left"
+        icon="translate"></b-icon>
+    </a>
+
+    <b-dropdown-item
+      v-for="lang in locales"
+      :key="lang.locale"
+      :class="{ 'language-active': $i18n.locale === lang.locale }"
+      aria-role="listitem"
+      @click="changeLocale(lang.locale)">
+      <div class="media">
+        <img
+          class="media-left"
+          :src="`/img/locales/${lang.locale}.png`"
+          width="30">
+        <h3>{{ lang.name }}</h3>
+      </div>
+    </b-dropdown-item>
+  </b-dropdown>
+</template>
+
+<script>
+  export default {
+    props: {
+      navbar: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data () {
+      return {
+        locales: [
+          { locale: 'en', name: 'English' }
+        ]
+      }
+    },
+    methods: {
+      changeLocale(locale) {
+        this.$i18n.locale = locale
+        localStorage.setItem('language', locale)
+        this.$emit('change', locale)
+      }
+    }
+  }
+</script>
+
+<style>
+.language-active {
+    background-color: #f5f5f5;
+    color: #1b1b1b;
+}
+</style>
