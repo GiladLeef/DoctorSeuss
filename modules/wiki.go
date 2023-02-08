da9cae5a9eb32f9afd188587e7dbc3b380a220b1
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2023-02-08 10:05:27 +0200

    Update wiki.go

--- modules/wiki.go
+++ modules/wiki.go
@@ -1,20 +1,48 @@
 package modules
 
 import (
-	"fmt"
-	"strings"
-	"github.com/trietmn/go-wiki"
+  "fmt"
+  "strings"
+  "regexp"
+  "github.com/trietmn/go-wiki"
 )
 
 var WikiTag = "wiki"
 
 func WikiReplacer(locale, entry, response, _ string) (string, string) {
 
-	res, err := gowiki.Summary(entry, 2, -1, false, true)
+  res, err := gowiki.Summary(entry, 2, -1, false, true)
     if err != nil {
-		responseTag := "no wiki"
-		return WikiTag, responseTag
+    responseTag := "no wiki"
+    return WikiTag, responseTag
     }
-    res = strings.Replace(res, "(listen)", "", -1)
-	return WikiTag, fmt.Sprintf(response, res)
+  
+  re := regexp.MustCompile(\[[^\]]*\])
+  output := re.ReplaceAllString(res, "")
+
+  // Remove text inside parentheses
+  re = regexp.MustCompile(\([^\)]*\))
+  output = re.ReplaceAllString(output, "")
+  
+  // Remove (born XX Month YYYY)
+  re = regexp.MustCompile(\(born [0-9]+ [A-Z][a-z]+ [0-9]+\))
+  output = re.ReplaceAllString(output, "")
+
+    // Remove (XX Month YYYY – XX Month YYYY)
+  re = regexp.MustCompile(\([0-9]+ [A-Z][a-z]+ [0-9]+ – [0-9]+ [A-Z][a-z]+ [0-9]+\))
+    output = re.ReplaceAllString(output, "")
+
+  // Remove text inside slashes
+  re = regexp.MustCompile(/[^/]*/)
+  output = re.ReplaceAllString(output, "")
+
+  // Remove semi-colons
+  output = strings.Replace(output, ";", "", -1)
+    res = strings.Replace(output, "(listen)", "", -1)
+
+  // Remove all non-English characters
+  re = regexp.MustCompile([^\x00-\x7F]+)
+  output = re.ReplaceAllString(output, "")
+  
+  return WikiTag, fmt.Sprintf(response, res)
 }
